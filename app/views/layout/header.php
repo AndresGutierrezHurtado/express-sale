dea457c0b045eecb97757cf1d84df90d5698e76c
Juan Hernandez <hernandezjuandavidguerrero@gmail.com>
2024-06-25 15:20:04 -0500

    some changes done on user profile section

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -10,6 +10,8 @@
                 </a>
                 <button class="px-3 sm:hidden" id="dropdown-menu-button"><i class="fa-solid fa-bars"></i></button>
             </div>
+
+            <!-- Search form -->
             <form class="hidden md:block w-5/12" action="/page/products/" method="GET" >
                 <div class="flex justify-center">
                     <?php if (isset($_GET['search']) && !empty($_GET['search'])) : ?>
@@ -19,19 +21,41 @@
                     <button class="border rounded-r-xl border-gray-800 bg-gray-800 text-white rounded-r-md p-1 px-3"><i class="fa-solid fa-magnifying-glass"></i></button>
                 </div>
             </form>
+
+            <!-- Account buttons -->
             <div class="flex gap-4">
                 <?php if (isset($_SESSION['user_username'])): ?> 
-                    <a href="/page/user_profile" class="flex gap-3 items-center"> <i class="fa-regular fa-circle-user text-[25px]"></i> Mi cuenta </a> 
-                    <a href="/page/user_cart" class="flex gap-3 items-center"> <i class="fa-solid fa-cart-shopping text-[15px]"></i> Carrito </a>                 
+                    <a href="/page/user_profile" class="flex gap-3 items-center"> 
+                        <i class="fa-regular fa-circle-user text-[25px]"></i> 
+                        Mi cuenta 
+                    </a> 
+                    <a href="/page/user_cart" class="flex gap-3 items-center"> 
+                        <i class="fa-solid fa-cart-shopping text-[15px]"></i> 
+                        Carrito 
+                    </a>                 
                 <?php else: ?>
-                    <a href="/page/login" class="flex gap-3 items-center"> <i class="fa-solid fa-right-to-bracket text-[18px]"></i> Registro </a> 
+                    <a href="/page/login" class="flex gap-3 items-center"> 
+                        <i class="fa-solid fa-right-to-bracket text-[18px]"></i> 
+                        Registro 
+                    </a> 
                 <?php endif; ?>
                 
                 <?php if (isset($_SESSION['user_role_id']) && $_SESSION['user_role_id'] == 3): ?> 
-                    <a href="/page/delivery_list" class="flex gap-3 items-center"> <i class="fa-solid fa-truck text-[15px]"></i> Envíos </a>
+                    <a href="/page/delivery_list" class="flex gap-3 items-center"> 
+                        <i class="fa-solid fa-truck text-[15px]"></i> 
+                        Envíos 
+                    </a>
+                <?php endif; ?>
+                
+                <?php if (isset($_SESSION['user_role_id']) && $_SESSION['user_role_id'] == 4): ?> 
+                    <a href="/page/dashboard_users" class="flex gap-3 items-center"> 
+                        <i class="fa-solid fa-gear text-[15px]"></i> 
+                        Administrador 
+                    </a>
                 <?php endif; ?>
             </div>
         </span>
+
         <!-- Header bottom section  -->
         <ul class="w-full sm:w-fit mx-auto hidden sm:block" id="dropdown-ul">
             <li class="flex flex-col sm:flex-row w-full text-center gap-1 sm:gap-4 text-[18px] mx-auto">
--- app/views/profile/user.view.php
+++ app/views/profile/user.view.php
@@ -7,71 +7,102 @@
     <link rel="shortcut icon" href="/public/images/logo.png" type="image/png">
     <script src="https://kit.fontawesome.com/eb36e646d1.js" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        th, td {
+            padding: 8px;
+        }
+    </style>
 </head>
 <body class="w-full min-h-screen bg-gray-50">
+    <!-- Header -->
     <?php require_once(__DIR__ . "/../layout/header.php") ?>
     <main class="w-full flex flex-col gap-10 py-12 justify-center items-center container mx-auto px-5">
+
         <form id="user_profile_form" class="w-full flex flex-col md:flex-row justify-between items-start gap-5" enctype="multipart/form-data">
             <input type="hidden" class="hidden" name="user_id" id="user_id" value="<?= isset($_GET['id']) ? $_GET['id'] : $_SESSION['user_id'] ?>">
+            
+            <!-- Image container -->
+            <div class="bg-white flex flex-col gap-4 p-5 rounded-lg shadow-lg w-full w-full md:max-w-[350px] mx-auto">
 
-            <div class="bg-white flex flex-col gap-4 p-5 rounded-lg shadow-lg w-full w-full sm:w-[300px] mx-auto">
                 <h1 class="text-lg uppercase tracking-tight font-bold">Imagen:</h1>
-                <div class="w-full aspect-square [background-image:url('<?= $user['image_url'] ?>')] bg-cover bg-center rounded-lg shadow-lg"> </div>
+
+                <div class="w-full aspect-square overflow-hidden rounded-md shadow-lg">
+                    <img src="<?= $user['image_url'] ?>" alt="foto <?= $user['user_first_name'] ?>"
+                    class="object-cover h-full w-full">
+                </div>
+
                 <input type="file" id="image" name="image" 
                 class="hidden w-full text-sm text-slate-500 hover:file:bg-violet-100 file:duration-300 file:cursor-pointer file:bg-violet-50 file:text-violet-700 file:font-semibold file:rounded-xl file:border-0 file:p-1 file:px-3">
-                <button id="btn-edit" class="text-violet-800 border-2 border-violet-800 py-2 px-4 rounded-md mt-auto w-max font-bold cursor-pointer">Editar</button>
-                <a class="bg-violet-800 text-white py-2 px-4 rounded-md mt-auto w-max font-bold cursor-pointer" id="btn-logout">Cerrar sesión</a>
+                <button id="btn-edit" class="text-violet-800 border-2 border-violet-800 py-2 px-4 rounded-md mt-auto w-max font-bold cursor-pointer">
+                <i class="fa-solid fa-pen mr-2"></i>
+                    Editar
+                </button>
+                <a class="bg-violet-800 text-white py-2 px-4 rounded-md mt-auto w-max font-bold cursor-pointer" id="btn-logout">
+                    <i class="fa-solid fa-right-from-bracket mr-2"></i>
+                    Cerrar sesión
+                </a>
+                
             </div>
 
-            <div class="bg-white p-5 rounded-lg shadow-lg flex flex-col gap-4 w-full md:w-6/12 lg:w-9/12">
+            <!-- Information container -->
+            <div class="bg-white p-5 rounded-lg shadow-lg flex flex-col gap-4 w-full md:max-w-[1fr]">
+
                 <h1 class="text-lg uppercase tracking-tight font-bold">Datos de <?= $user['user_username'] ?>:</h1>
+
                 <div class="flex flex-col gap-1">
                     <label for="full_name" class="text-md font-medium text-gray-700">Nombres:</label>
-                    <input type="text" id="full_name" name="user_first_name" value="<?= $user['user_first_name']; ?>" class="w-full border rounded-lg py-1 px-3" disabled>
+                    <input type="text" id="full_name" name="user_first_name" value="<?= $user['user_first_name']; ?>" 
+                    class="w-full border rounded-lg py-1 px-3" disabled>
                 </div>
+
                 <div class="flex flex-col gap-1">
                     <label for="full_name" class="text-md font-medium text-gray-700">Apellidos:</label>
-                    <input type="text" id="full_name" name="user_last_name" value="<?= $user['user_last_name']; ?>" class="w-full border rounded-lg py-1 px-3" disabled>
+                    <input type="text" id="full_name" name="user_last_name" value="<?= $user['user_last_name']; ?>" 
+                    class="w-full border rounded-lg py-1 px-3" disabled>
                 </div>
+
                 <div class="flex flex-col gap-1">
                     <label for="username" class="text-md font-medium text-gray-700">Nombre de Usuario:</label>
-                    <input type="text" id="username" name="user_username" value="<?= $user['user_username']; ?>" class="w-full border rounded-lg py-1 px-3" disabled>
+                    <input type="text" id="username" name="user_username" value="<?= $user['user_username']; ?>" 
+                    class="w-full border rounded-lg py-1 px-3" disabled>
                 </div>
 
                 <div class="flex flex-col gap-1">
                     <label for="email" class="text-md font-medium text-gray-700">Correo Electrónico:</label>
-                    <input type="email" id="email" name="user_email" value="<?= $user['user_email']; ?>" class="w-full border rounded-lg py-1 px-3" disabled>
+                    <input type="email" id="email" name="user_email" value="<?= $user['user_email']; ?>" 
+                    class="w-full border rounded-lg py-1 px-3" disabled>
                 </div>
 
                 <div class="flex flex-col gap-1">
                     <label for="address" class="text-md font-medium text-gray-700">Dirección:</label>
-                    <input type="text" id="address_input" name="user_address" value="<?= $user['user_address']; ?>" class="w-full border rounded-lg py-1 px-3" disabled autocomplete="off">
+                    <input type="text" id="address_input" name="user_address" value="<?= $user['user_address']; ?>" 
+                    class="w-full border rounded-lg py-1 px-3" disabled autocomplete="off">
                 </div>
 
                 <div class="flex flex-col gap-1">
                     <label for="phone_number" class="text-md font-medium text-gray-700">Número de Teléfono:</label>
-                    <input type="number" id="phone_number" name="user_phone_number" value="<?= $user['user_phone_number']; ?>" class="w-full border rounded-lg py-1 px-3" disabled>
+                    <input type="number" id="phone_number" name="user_phone_number" value="<?= $user['user_phone_number']; ?>" 
+                    class="w-full border rounded-lg py-1 px-3" disabled>
                 </div>
 
                 <?php if ($user['user_role_id'] == 2 || $user['user_role_id'] == 3):?>
                     <div class="flex flex-col gap-1">
                         <label for="description" class="text-md font-medium text-gray-700">Descripción:</label>
                         <textarea type="text" id="description" name="worker_description" 
-                        class="h-24 w-full border rounded-lg py-1 px-3 resize-none" disabled> <?= $user['worker_description'] ?> </textarea>
+                        class="h-24 w-full border rounded-lg py-1 px-3 resize-none" disabled> 
+                            <?= $user['worker_description'] ?> 
+                        </textarea>
                     </div>
                 <?php endif; ?>
 
                 <label for="account_type">Tipo de Cuenta: <strong class="capitalize"> <?= $user['role_name']?> </strong></label>
 
                 <span class="w-full flex justify-between items-center">
-                    <div>
-                        <button type="submit" id="btn-submit" name="submit" class="bg-violet-800 text-white py-2 px-4 rounded-md mt-auto w-max font-bold hidden">Actualizar Perfil</button>
-                    </div>
-                    <?php if ($_SESSION['user_role_id'] == 4):?>
-                        <div class="flex justify-end gap-5">
-                            <a href="/page/dashboard_users" class="bg-violet-800 text-white py-2 px-4 rounded-md mt-auto w-max font-bold"> Administrador </a>
-                        </div>
-                    <?php endif; ?>
+                    <button type="submit" id="btn-submit" name="submit" 
+                    class="bg-violet-800 text-white py-2 px-4 rounded-md mt-auto w-max font-bold hidden">
+                        <i class="fa-solid fa-arrow-up-from-bracket mr-2"></i>
+                        Actualizar Perfil
+                    </button>
                 </span>
             </div>
         </form>
@@ -85,27 +116,27 @@
                 <table class="w-full table-auto border-collapse border border-gray-900 text-[13px] sm:text-[16px] text-center max-w-full" >
                     <thead>
                         <tr class="bg-gray-200">
-                            <th class="p-2">Compra</th>
-                            <th class="p-2 hidden lg:table-cell">Productos</th>
-                            <th class="p-2">Fecha</th>
-                            <th class="p-2">Estado</th>
-                            <th class="p-2">Precio</th>
-                            <th class="p-2">Ver</th>
+                            <th>Fecha</th>
+                            <th class=" hidden lg:table-cell">Productos</th>
+                            <th>Estado</th>
+                            <th>Precio</th>
+                            <th>Ver</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php foreach ($orders as $order): ?>
                         <tr>
-                            <td class="p-2"><?= $order['order_id']; ?></td>
-                            <td class="p-2"><?php
-                            foreach ($order['products'] as $product) {
-                                echo $product['product_name'] . ' - '.$product['sold_product_quantity'].' ( producto/s.) <br>';
-                            }
-                            ?></td>
-                            <td class="p-2"><?= $order['order_date'] ?></td>
-                            <td class="p-2"> <?= $order['state_name'] ?> </td>
-                            <td class="p-2"><?= number_format($order['order_amount']); ?> COP</td>
-                            <td class="p-2"> 
+                            <td ><?= $order['order_date'] ?></td>
+                            <td >
+                                <ol>
+                                    <?php foreach ($order['products'] as $product) :?>
+                                        <li><?= $product['sold_product_quantity'] ?>  <?= $product['product_name'] ?></li> 
+                                    <?php endforeach; ?>
+                                </ol>
+                            </td>
+                            <td > <?= $order['state_name'] ?> </td>
+                            <td ><?= number_format($order['order_amount']); ?> COP</td>
+                            <td > 
                                 <a href="/page/order_shift/?id=<?= $order['order_id'] ?>">
                                     <button class="p-[2px] sm:px-3 border-2 border-violet-800 text-violet-800 rounded-md font-bold duration-300 hover:bg-gray-200">
                                         <i class="fa-regular fa-eye"></i>
@@ -136,23 +167,23 @@
                     <table class="w-full table-auto border-collapse border border-gray-900 text-[13px] sm:text-[16px] text-center max-w-full" >
                         <thead>
                             <tr class="bg-gray-200">
-                                <th class="p-2">ID</th>
-                                <th class="p-2 hidden lg:table-cell">Nombre</th>
-                                <th class="p-2">Precio</th>
-                                <th class="p-2 hidden sm:table-cell">Stock</th>
-                                <th class="p-2">Categoría</th>                            
-                                <th class="p-2">Acciones</th>
+                                <th>ID</th>
+                                <th class="hidden lg:table-cell">Nombre</th>
+                                <th>Precio</th>
+                                <th class="hidden sm:table-cell">Stock</th>
+                                <th>Categoría</th>                            
+                                <th>Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php foreach ( $products['data'] as $product ): ?>
                             <tr>
-                                <td class="p-2"><?= $product['product_id']; ?></td>
-                                <td class="p-2 hidden lg:table-cell"><?= $product['product_name']; ?></td>
-                                <td class="p-2"><?= number_format($product['product_price']); ?> COP</td>
-                                <td class="p-2 hidden sm:table-cell"><?= $product['product_stock']; ?></td>
-                                <td class="p-2"><?= $product['category_name'] ?> </td>
-                                <td class="p-2 flex flex-col gap-3 sm:flex-row justify-center"> 
+                                <td ><?= $product['product_id']; ?></td>
+                                <td class="hidden lg:table-cell"><?= $product['product_name']; ?></td>
+                                <td ><?= number_format($product['product_price']); ?> COP</td>
+                                <td class="hidden sm:table-cell"><?= $product['product_stock']; ?></td>
+                                <td ><?= $product['category_name'] ?> </td>
+                                <td class="flex flex-col gap-3 sm:flex-row justify-center"> 
                                     <a href="/page/product_profile/?id=<?= $product['product_id'] ?>"><button class="p-[2px] sm:px-3 border-2 border-violet-800 text-violet-800 rounded-md font-bold duration-300 hover:bg-gray-200"><i class="fa-solid fa-pen-to-square"></i></button></a>
                                     <button class="p-[2px] sm:px-3 bg-violet-800 text-white rounded-md font-bold duration-300 hover:bg-violet-600 btn-delete" onclick="deleteElement(<?= $product['product_id'] ?>)"><i class="fa-solid fa-trash-can"></i></button> 
                                 </td>
