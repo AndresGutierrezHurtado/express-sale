c7e2fdc55261771aa2b73f64e5bf2560b17c3e2a
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-11 01:39:56 -0500

    refactor: fix login function in add to cart buttons

--- app/views/pages/products.view.php
+++ app/views/pages/products.view.php
@@ -154,13 +154,17 @@
                             </span>
 
                             <!-- Agregar al carrito -->
-                            <button <?= isset($_SESSION['usuario_id']) ? "onclick=\"useFetch('/cart/update', 'POST', {producto_id: " . $product['producto_id'] . "} )\" " : 'login()' ?>
-                                class="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-600 px-4 py-2 text-sm font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 disabled:cursor-wait disabled:opacity-50">
-                                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <i class="fa-solid fa-cart-plus text-[17px] text-violet-500 duration-300 group-hover:text-violet-400"></i>
-                                </span>
-                                Agregar al carrito
-                            </button>
+                            <form action="/cart/update" method="post" class="fetch-form w-full">
+                                <input type="hidden" name="producto_id" value="<?= $product['producto_id'] ?>">
+
+                                <button <?= isset($_SESSION['usuario_id']) ? "type='submit'" : "type='button' onclick='login()'" ?>
+                                    class="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-600 px-4 py-2 text-sm font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 disabled:cursor-wait disabled:opacity-50">
+                                    <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                        <i class="fa-solid fa-cart-plus text-[17px] text-violet-500 duration-300 group-hover:text-violet-400"></i>
+                                    </span>
+                                    Agregar al carrito
+                                </button>
+                            </form>
                         </div>
                     </article>
 
