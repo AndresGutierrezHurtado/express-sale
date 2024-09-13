2b182c4a603627bfb37793f0cd372fb19496f731
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-12 19:04:31 -0500

    change: confirm functions to confirmAlert from SweetAlert2

--- app/views/pages/admin/dashboard_users.view.php
+++ app/views/pages/admin/dashboard_users.view.php
@@ -92,7 +92,7 @@
                                         <form action="/user/delete" method="post" class="fetch-form">
                                             <input type="hidden" name="usuario_id" value="<?= $user['usuario_id'] ?>">
 
-                                            <button type="submit" onclick="return confirm('¿Esta seguro de eliminar este usuario?')" class="relative group flex items-center justify-center rounded-md border border-transparent bg-violet-600 px-4 py-2 text-sm font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 disabled:cursor-wait disabled:opacity-50 cursor-pointer">
+                                            <button type="submit" onclick="confirmAlert(event, '¿Esta seguro de eliminar este usuario?')" class="relative group flex items-center justify-center rounded-md border border-transparent bg-violet-600 px-4 py-2 text-sm font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 disabled:cursor-wait disabled:opacity-50 cursor-pointer">
                                                 <i class="fa-solid fa-trash-can text-[18px] text-violet-400 duration-300 group-hover:text-violet-300"></i>
                                             </button>
                                         </form>
--- app/views/pages/seller.view.php
+++ app/views/pages/seller.view.php
@@ -131,7 +131,7 @@
                                                     <li onclick="seller_modal_<?= $calification['calificacion_id'] ?>.showModal()"><a class="text-center flex justify-center gap-2"> <i class="fa-solid fa-pen text-sm"></i>Editar</a></li>
                                                     <form class="fetch-form w-full text-red-500" action="/calification/delete" method="post">
                                                         <input type="hidden" name="calificacion_id" value="<?= $calification['calificacion_id'] ?>">
-                                                        <button type="submit" class="w-full" onclick="return confirm('¿Seguro que quieres eliminar este comentario?, esta acción es irreversible.')">
+                                                        <button type="submit" class="w-full" onclick="confirmAlert(event, '¿Seguro que quieres eliminar este comentario?, esta acción es irreversible.')">
                                                             <li>
                                                                 <a class="text-center flex justify-center gap-2">
                                                                     <i class="fa-solid fa-trash-can text-sm"></i>Eliminar
