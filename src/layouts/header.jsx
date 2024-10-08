7959b1945df5486d956121bbc4b942dc190a6d08
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-10-07 23:51:18 -0500

    feat: add components and login and register

---
+++ src/layouts/header.jsx
@@ -0,0 +1,107 @@
+import { Link } from "react-router-dom";
+import { IoSearch as Search, IoMenu as Menu } from "react-icons/io5";
+import { MdOutlineShoppingBag as ShoppingBag } from "react-icons/md";
+
+export default function Header() {
+    return (
+        <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                {/* Navbar */}
+                <div className="w-full max-w-[1200px] mx-auto bg-base-100">
+                    <div className="navbar">
+                        <div className="flex-none lg:hidden">
+                            <label
+                                for="my-drawer-3"
+                                aria-label="open sidebar"
+                                className="btn btn-square btn-ghost"
+                            >
+                                <Menu size={23} />
+                            </label>
+                        </div>
+                        <div className="navbar-start">
+                            <Link to="/" className="btn btn-ghost text-xl">
+                                <img
+                                    src="/logo.png"
+                                    alt="logo express sale"
+                                    className="object-contain w-full h-full"
+                                />
+                            </Link>
+                        </div>
+                        <div className="navbar-center hidden lg:flex text-purple-700">
+                            <ul className="menu menu-horizontal px-1 text-[1.05rem]">
+                                <li>
+                                    <Link to="/">Inicio</Link>
+                                </li>
+                                <li>
+                                    <Link to="/products">Productos</Link>
+                                </li>
+                                <li>
+                                    <Link to="/#contact">Contacto</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="navbar-end gap-2 [&_svg]:text-purple-700">
+                            <button className="btn btn-ghost btn-circle">
+                                <Search size={25} />
+                            </button>
+                            <div className="dropdown dropdown-end">
+                                <div
+                                    tabindex="0"
+                                    role="button"
+                                    className="btn btn-ghost btn-circle avatar"
+                                >
+                                    <div className="w-10 rounded-full">
+                                        <img
+                                            alt="Tailwind CSS Navbar component"
+                                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                        />
+                                    </div>
+                                </div>
+                                <ul
+                                    tabindex="0"
+                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                                >
+                                    <li>
+                                        <Link to="/login">Iniciar sesión</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/register">Registrarse</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/user/profile">Perfil</Link>
+                                    </li>
+                                    <li>
+                                        <a>Cerrar sesión</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <button className="btn btn-ghost btn-circle">
+                                <ShoppingBag size={27} />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="drawer-side">
+                <label
+                    htmlFor="my-drawer-3"
+                    aria-label="close sidebar"
+                    className="drawer-overlay"
+                ></label>
+                <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                    {/* Sidebar content here */}
+                    <li>
+                        <Link to="/">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Productos</Link>
+                    </li>
+                    <li>
+                        <Link to="/#contact">Contacto</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/auth/register.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { TbLogin2 as LoginIcon } from "react-icons/tb";
+import { FaArrowUpFromBracket as RegisterIcon } from "react-icons/fa6";
+
+export default function Register() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = Object.fromEntries(new FormData(event.target));
+
+    }
+
+    return (
+        <section className="hero bg-base-100 min-h-screen w-full">
+            <div className="fixed inset-0 overflow-hidden">
+                <div className="absolute bg-purple-700 h-[120%] md:h-[140vh] w-[100%] lg:right-[-50%] rounded-full top-[-10%] lg:top-1/2 transform -translate-y-1/2"></div>
+            </div>
+            <div className="hero-content flex-col lg:flex-row-reverse lg:[&>*]:w-1/2 [&>*]:w-full max-w-[1200px] w-full px-3 gap-10">
+                <div className="text-center lg:text-right text-base-100 space-y-2">
+                    <h1 className="text-3xl lg:text-4xl font-bold">
+                        ¿Ya tienes una cuenta?
+                    </h1>
+                    <p className="pb-5">
+                        Si ya tienes una cuenta puedes iniciar sesión en el
+                        siguiente botón.
+                    </p>
+                    <Link
+                        to="/login"
+                        className="btn btn-ghost border border-white"
+                    >
+                        <LoginIcon />
+                        Inicia sesión
+                    </Link>
+                </div>
+
+                <div className="card bg-base-100 w-full max-w-[500px] shrink-0 shadow-2xl">
+                    <form className="card-body" onSubmit={handleSubmit}>
+                        <div className="form-control text-center">
+                            <figure className="size-[120px] mx-auto">
+                                <img
+                                    src="/logo.png"
+                                    alt="logo express sale"
+                                    className="object-contain w-full h-full"
+                                />
+                            </figure>
+                            <h2 className="text-2xl font-bold">Regístrate</h2>
+                            <p>
+                                Crea una nueva cuenta para ser parte de nuestra
+                                comunidad.
+                            </p>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
+                                    Nombres:
+                                </span>
+                            </label>
+                            <input
+                                placeholder="Ingresa tus nombres"
+                                className="input focus:input-primary input-bordered focus:outline-0"
+                                name="usuario_nombre"
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
+                                    Apellidos:
+                                </span>
+                            </label>
+                            <input
+                                placeholder="Ingresa tus apellidos"
+                                className="input focus:input-primary input-bordered focus:outline-0"
+                                name="usuario_apellido"
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
+                                    Usuario:
+                                </span>
+                            </label>
+                            <input
+                                placeholder="Ingresa un alias único"
+                                className="input focus:input-primary input-bordered focus:outline-0"
+                                name="usuario_alias"
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
+                                    Correo Electronico:
+                                </span>
+                            </label>
+                            <input
+                                placeholder="ejemplo@gmail.com"
+                                className="input focus:input-primary input-bordered focus:outline-0"
+                                name="usuario_correo"
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
+                                    Rol
+                                </span>
+                            </label>
+                            <select
+                                className="select select-bordered focus:outline-0 focus:input-primary text-gray-600"
+                                name="rol_id"
+                            >
+                                <option value="">-- Seleccionar --</option>
+                                <option value="1">Usuario</option>
+                                <option value="2">Vendedor</option>
+                                <option value="3">Domiciliario</option>
+                            </select>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
+                                    Contraseña:
+                                </span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="******"
+                                className="input focus:input-primary input-bordered focus:outline-0"
+                                name="usuario_contra"
+                            />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary group relative text-purple-300 hover:bg-purple-800 hover:text-purple-100">
+                                <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-purple-300 group-hover:text-purple-100">
+                                    <RegisterIcon size={15} />
+                                </span>
+                                Regístrate
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+}
