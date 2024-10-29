e624b185391cdd4a4484888a4911dbc4fc8bc4ed
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-10-29 01:02:50 -0500

    build: install new dependences

---
+++ server/config/uploadImage.js
@@ -0,0 +1,82 @@
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+
+// Cloudinary
+(async function () {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    const uploadImage = await cloudinary.uploader
+        .upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg", {
+            public_id: "shoes",
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    const optimizeUrl = cloudinary.url("shoes", {
+        fetch_format: "auto",
+        quality: "auto",
+    });
+})();
+
+const storage = multer.memoryStorage();
+
+export const upload = multer({ storage });
+
+export const uploadFile = async (file, id, type = "image") => {
+    try {
+        const base64Data = file.replace(/^data:image\/\w+;base64,/, '');
+        const buffer = Buffer.from(base64Data, 'base64');
+
+        const result = await cloudinary.uploader.upload_stream(
+            {
+                public_id: id,
+                overwrite: true,
+                unique_filename: false,
+                resource_type: type == "image" ? "image" : "video",
+                format: type == "image" ? "jpg" : "mp4",
+                transformation: [
+                    { quality: "auto:sensitive", fetch_format: "auto" }
+                ]
+            },
+            (error, result) => {
+                if (error) throw new Error(error.message);
+                return result;
+            }
+        );
+
+        buffer.pipe(result);
+        return {
+            success: true,
+            message: "Imagen subida correctamente",
+            data: result
+        };
+    } catch (error) {
+        return {
+            success: false,
+            message: error.message,
+            data: null
+        };
+    }
+};
+
+export const deleteFile = (public_id) => {
+    cloudinary.uploader.destroy(public_id, (error, result) => {
+        if (error) {
+            return {
+                success: false,
+                message: error.message,
+                data: null,
+            };
+        }
+
+        return {
+            success: true,
+            message: "Imagen eliminada correctamente",
+            data: result,
+        };
+    });
+};
