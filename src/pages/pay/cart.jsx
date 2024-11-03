f69099bc3aff40fc64205b75810704fb43d3279b
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-11-02 20:10:30 -0500

    feat: cart page working

--- src/pages/pay/cart.jsx
+++ src/pages/pay/cart.jsx
@@ -1,8 +1,81 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
-import { TrashIcon, PlusIcon, MinusIcon } from "@components/icons";
+// Components
+import ContentLoading from "@components/contentLoading.jsx";
+import { TrashIcon, PlusIcon, MinusIcon } from "@components/icons.jsx";
+
+// Hooks
+import { useDeleteData, useGetData, usePutData } from "@hooks/useFetchData.js";
+
+// Contexts
+import { useAuthContext } from "@contexts/authContext.jsx";
 
 export default function Cart() {
+    const { userSession, loading: loadingUserSession } = useAuthContext();
+    const {
+        data: carts,
+        loading: loadingCarts,
+        reload: reloadCarts,
+    } = useGetData(`/users/${userSession.usuario_id}/carts`);
+
+    const handleCartUpdate = async (id, quantity) => {
+        const response = await usePutData(`/carts/${id}`, { producto_cantidad: quantity });
+        if (response.success) {
+            reloadCarts();
+        }
+    };
+
+    const handleCartDelete = async (event) => {
+        const { id } = event.target.dataset;
+        Swal.fire({
+            icon: "warning",
+            title: "¿Estas seguro?",
+            text: "Dale a continuar si quieres eliminar el producto",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            cancelButtonText: "Cancelar",
+            confirmButtonText: "Continuar",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const response = await useDeleteData(`/carts/${id}`);
+                if (response.success) {
+                    reloadCarts();
+                }
+            }
+        })
+    };
+
+    const handleEmptyCart = async () => {
+        Swal.fire({
+            icon: "warning",
+            title: "¿Estas seguro?",
+            text: "Dale a continuar si quieres vaciar el carrito",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            cancelButtonText: "Cancelar",
+            confirmButtonText: "Continuar",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const response = await useDeleteData(
+                    `/users/${userSession.usuario_id}/carts/empty`
+                );
+                if (response.success) {
+                    reloadCarts();
+                }
+            }
+        });
+    };
+
+    const subTotal = carts?.reduce(
+        (acc, cart) => acc + cart.product.producto_precio * cart.producto_cantidad,
+        0
+    );
+
+    if (loadingCarts) return <ContentLoading />;
     return (
         <>
             <section className="w-full px-3">
@@ -10,80 +83,131 @@ export default function Cart() {
                     <div className="space-y-5">
                         <h2 className="text-3xl font-extrabold text-center">Carrito de compras</h2>
                         <div className="flex gap-10">
-                            <div className="grow">
-                                <article className="bg-white p-8 w-full shadow-xl rounded-xl border">
-                                    <div className="flex gap-5">
-                                        <figure className="size-[160px]">
-                                            <img
-                                                src="https://res.cloudinary.com/dyuh7jesr/image/upload/v1730360610/express-sale/products/09fd4c7b-cd37-4e6d-a7f6-ae8f93734005.jpg"
-                                                alt="Imagen producto"
-                                                className="object-contain h-full w-full"
-                                            />
-                                        </figure>
-                                        <div className="grow h-[initial]">
-                                            <div className="flex w-full h-full">
-                                                <div className="grow h-full flex flex-col justify-between">
-                                                    <div className="grow">
-                                                        <h2 className="text-3xl font-bold">
-                                                            Producto
-                                                        </h2>
-                                                        <p className="text-lg font-bold">
-                                                            10.000 COP
-                                                        </p>
-                                                        <p className="text-sm font-semibold text-gray-600">
-                                                            Cant: 1
-                                                        </p>
+                            <div className="grow space-y-5">
+                                {carts.length === 0 && (
+                                    <h2 className="text-xl font-bold py-8">
+                                        Tu carrito de compras esta vacio...
+                                    </h2>
+                                )}
+                                {carts.map((cart) => (
+                                    <article
+                                        key={cart.carrito_id}
+                                        className="bg-white p-8 w-full shadow-xl rounded-xl border"
+                                    >
+                                        <div className="flex gap-5">
+                                            <Link to={`/product/${cart.producto_id}`} className="size-[160px]">
+                                                <img
+                                                    src={cart.product.producto_imagen_url}
+                                                    alt={`Imagen del producto ${cart.product.producto_nombre}`}
+                                                    className="object-contain h-full w-full"
+                                                />
+                                            </Link>
+                                            <div className="grow h-[initial]">
+                                                <div className="flex w-full h-full">
+                                                    <div className="grow h-full flex flex-col justify-between">
+                                                        <div className="grow">
+                                                            <h2 className="text-3xl font-bold">
+                                                                {cart.product.producto_nombre}
+                                                            </h2>
+                                                            <p className="text-lg font-bold">
+                                                                {parseInt(
+                                                                    cart.product.producto_precio
+                                                                ).toLocaleString("es-CO")}{" "}
+                                                                COP
+                                                            </p>
+                                                            <p className="text-sm font-semibold text-gray-600">
+                                                                Cant: {cart.producto_cantidad}
+                                                            </p>
+                                                        </div>
+                                                        <span className="flex gap-4 items-center justify-start">
+                                                            <button
+                                                                className="btn btn-sm tooltip tooltip-top"
+                                                                data-tip="Disminuir la cantidad del producto (min 1)"
+                                                                onClick={() =>
+                                                                    handleCartUpdate(
+                                                                        cart.carrito_id,
+                                                                        cart.producto_cantidad - 1
+                                                                    )
+                                                                }
+                                                                disabled={
+                                                                    cart.producto_cantidad === 1
+                                                                }
+                                                            >
+                                                                <MinusIcon size={12} />
+                                                            </button>
+                                                            <p className="text-xl font-medium">
+                                                                {cart.producto_cantidad}
+                                                            </p>
+                                                            <button
+                                                                className="btn btn-sm tooltip tooltip-top"
+                                                                data-tip={`Aumentar la cantidad del producto (max ${cart.product.producto_cantidad})`}
+                                                                onClick={() =>
+                                                                    handleCartUpdate(
+                                                                        cart.carrito_id,
+                                                                        cart.producto_cantidad + 1
+                                                                    )
+                                                                }
+                                                                disabled={
+                                                                    cart.producto_cantidad ===
+                                                                    cart.product.producto_cantidad
+                                                                }
+                                                            >
+                                                                <PlusIcon />
+                                                            </button>
+                                                        </span>
                                                     </div>
-                                                    <span className="flex gap-4 items-center justify-start">
+                                                    <div className="text-end flex flex-col justify-between items-end h-full">
+                                                        <p className="text-2xl font-bold">
+                                                            {parseInt(
+                                                                cart.product.producto_precio *
+                                                                    cart.producto_cantidad
+                                                            ).toLocaleString("es-CO")}{" "}
+                                                            COP
+                                                        </p>
                                                         <button
-                                                            className="btn btn-sm tooltip tooltip-top"
-                                                            data-tip="Disminuir la cantidad del producto"
+                                                            className="tooltip tooltip-left btn btn-sm bg-red-600 hover:bg-red-700 text-red-400 hover:text-red-300"
+                                                            data-tip="Eliminar el producto del carrito"
+                                                            data-id={cart.carrito_id}
+                                                            onClick={handleCartDelete}
                                                         >
-                                                            <MinusIcon size={12} />
+                                                            <TrashIcon size={17} />
                                                         </button>
-                                                        <p className="text-xl">4</p>
-                                                        <button
-                                                            className="btn btn-sm tooltip tooltip-top"
-                                                            data-tip="Aumentar la cantidad del producto"
-                                                        >
-                                                            <PlusIcon />
-                                                        </button>
-                                                    </span>
-                                                </div>
-                                                <div className="text-end flex flex-col justify-between items-end h-full">
-                                                    <p className="text-2xl font-bold">40.000</p>
-                                                    <button
-                                                        className="tooltip tooltip-left btn btn-sm bg-red-600 hover:bg-red-700 text-red-400 hover:text-red-300"
-                                                        data-tip="Eliminar el producto del carrito"
-                                                    >
-                                                        <TrashIcon size={17} />
-                                                    </button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </article>
+                                    </article>
+                                ))}
                             </div>
-                            <div className="w-full max-w-[300px] p-8 space-y-5 bg-white border shadow-xl rounded-xl">
+                            <div className="w-full max-w-[300px] p-8 space-y-5 bg-white border shadow-xl rounded-xl h-fit">
                                 <div className="[&_div]:flex [&_div]:gap-5 [&_div]:justify-between [&_div]:w-full">
                                     <div>
                                         <h2 className="text-2xl font-bold">Resumen:</h2>
                                     </div>
                                     <div>
                                         <label>Subtotal</label>
-                                        <p>30.000</p>
+                                        <p>{parseInt(subTotal).toLocaleString("es-CO")}</p>
                                     </div>
                                     <div>
                                         <label>Iva</label>
-                                        <p>30.000</p>
+                                        <p>
+                                            19% ({parseInt(subTotal * 0.19).toLocaleString("es-CO")}
+                                            )
+                                        </p>
                                     </div>
                                     <div>
                                         <label>Total</label>
-                                        <p>30.000</p>
+                                        <p>{parseInt(subTotal * 1.19).toLocaleString("es-CO")}</p>
                                     </div>
-                                    <div className="form-group pt-5">
+                                    <div className="form-control pt-5">
                                         <button className="btn btn-primary btn-sm w-full">
                                             Pagar
+                                        </button>
+                                        <button
+                                            onClick={handleEmptyCart}
+                                            className="btn btn-sm w-full"
+                                        >
+                                            Vaciar carrito
                                         </button>
                                     </div>
                                 </div>
