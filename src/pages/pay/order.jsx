5c3f05fde55647bde1992abd5ffe8c8f75615af9
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-11-05 12:08:40 -0500

    style: improve order page style

--- src/pages/pay/order.jsx
+++ src/pages/pay/order.jsx
@@ -6,24 +6,86 @@ import { useGetData } from "@hooks/useFetchData.js";
 
 // Components
 import ContentLoading from "@components/contentLoading.jsx";
+import { EyeIcon, CircleCheckIcon, ArrowLeftIcon, ReceiptIcon } from "@components/icons.jsx";
 
 export default function Order() {
     const { id } = useParams();
     const { data: order, loading: orderLoading } = useGetData(`/orders/${id}`);
 
-    console.log(order);
     if (orderLoading) return <ContentLoading />;
     return (
         <>
             <div className="w-full min-h-screen flex items-center justify-center">
-                <div className="card bg-white border border-gray-100 shadow-lg rounded-lg w-full max-w-xl">
+                <div className="card bg-white border border-gray-100 shadow-lg rounded-lg w-full max-w-2xl">
                     <div className="card-body space-y-4">
                         <h2 className="text-4xl font-extrabold tracking-tight">
                             Detalles del pedido:
                         </h2>
-                        <div>
-                            <p>info...</p>
-                            <button className="btn w-full btn-sm">Ver Factura</button>
+                        <div className="space-y-4">
+                            <div className="flex [&>div]:grow gap-5">
+                                <article className="[&>div]:text-sm">
+                                    <h2 className="text-xl font-bold">Comprador:</h2>
+                                    <div className="flex gap-2">
+                                        <span className="font-medium">Nombre:</span>
+                                        <p>{order.paymentDetails.comprador_nombre}</p>
+                                    </div>
+                                    <div className="flex gap-2">
+                                        <span className="font-medium">Correo Electrónico:</span>
+                                        <p>{order.paymentDetails.comprador_correo}</p>
+                                    </div>
+                                    <div className="flex gap-2">
+                                        <span className="font-medium">Teléfono:</span>
+                                        <p>{order.paymentDetails.comprador_telefono}</p>
+                                    </div>
+                                </article>
+                                <article className="[&>div]:text-sm">
+                                    <h2 className="text-xl font-bold">Compra:</h2>
+                                    <div className="flex gap-2">
+                                        <span className="font-medium">Método de pago:</span>
+                                        <p>
+                                            {order.paymentDetails.pago_metodo === 2
+                                                ? "Tarjetas de Crédito"
+                                                : order.paymentDetails.pago_metodo === 4
+                                                ? "Transferencias bancarias PSE"
+                                                : order.paymentDetails.pago_metodo === 5
+                                                ? "Débitos ACH"
+                                                : order.paymentDetails.pago_metodo === 6
+                                                ? "Tarjetas débito"
+                                                : order.paymentDetails.pago_metodo === 7
+                                                ? "Efectivo"
+                                                : order.paymentDetails.pago_metodo === 8
+                                                ? "Referencia de pago"
+                                                : order.paymentDetails.pago_metodo === 10
+                                                ? "Pago en bancos"
+                                                : order.paymentDetails.pago_metodo === 14
+                                                ? "Transferencias bancarias SPEI"
+                                                : "Código no reconocido"}
+                                        </p>
+                                    </div>
+
+                                    <div className="flex gap-2">
+                                        <span className="font-medium">Total:</span>
+                                        <p>
+                                            {parseInt(
+                                                order.paymentDetails.pago_valor
+                                            ).toLocaleString("es-CO")}{" "}
+                                            COP
+                                        </p>
+                                    </div>
+
+                                    <div className="flex gap-2">
+                                        <span className="font-medium">Repartidor:</span>
+                                        <p>{order.shippingDetails.trabajador_id || "Pendiente"}</p>
+                                    </div>
+                                </article>
+                            </div>
+
+                            <button className="btn w-full btn-sm relative">
+                                <span className="absolute left-3 top-1/2 -translate-y-1/2">
+                                    <ReceiptIcon size={17} />
+                                </span>
+                                Ver factura
+                            </button>
                         </div>
                         <hr />
                         <div className="space-y-5">
@@ -47,9 +109,20 @@ export default function Order() {
                                             </figure>
                                             <div className="grow flex flex-col gap-1">
                                                 <div className="grow">
-                                                    <h2 className="text-xl font-bold tracking-tight">
-                                                        {product.product.producto_nombre}
-                                                    </h2>
+                                                    <div className="flex justify-between items-center w-full">
+                                                        <h2 className="text-xl font-bold tracking-tight">
+                                                            {product.product.producto_nombre}
+                                                        </h2>
+                                                        <Link
+                                                            to={`/product/${product.producto_id}`}
+                                                            data-tip="Ver perfil del producto"
+                                                            className="tooltip tooltip-left"
+                                                        >
+                                                            <button className="btn btn-sm btn-primary w-fit">
+                                                                <EyeIcon size={20} />
+                                                            </button>
+                                                        </Link>
+                                                    </div>
                                                     <p>cantidad: {product.producto_cantidad}</p>
                                                 </div>
                                                 <span className="flex justify-between items-center w-full">
@@ -69,14 +142,6 @@ export default function Order() {
                                                         COP
                                                     </p>
                                                 </span>
-                                                <div>
-                                                    <Link
-                                                        to={`/product/${product.producto_id}`}
-                                                        className="btn btn-sm btn-primary w-full"
-                                                    >
-                                                        Ver perfil
-                                                    </Link>
-                                                </div>
                                             </div>
                                         </article>
                                     </div>
@@ -84,9 +149,21 @@ export default function Order() {
                             </div>
                         </div>
                         <hr />
-                        <div className="space-y-2">
-                            <button className="btn w-full btn-sm btn-primary">Marcar como recibido</button>
-                            <Link to={`/profile/user/${order.usuario_id}`} className="btn w-full btn-sm">Volver</Link>
+                        <div className="flex flex-col gap-2">
+                            <button className="btn w-full btn-sm btn-success text-white relative">
+                                <span className="absolute left-3 top-1/2 -translate-y-1/2">
+                                    <CircleCheckIcon size={17} />
+                                </span>
+                                Marcar como recibido
+                            </button>
+                            <Link to={`/profile/user/${order.usuario_id}`}>
+                                <button className="btn w-full btn-sm relative">
+                                    <span className="absolute left-3 top-1/2 -translate-y-1/2">
+                                        <ArrowLeftIcon size={17} />
+                                    </span>
+                                    Volver
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
