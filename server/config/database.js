5e231b351272076b45b4478b91e0b86ff8bc4bc9
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-12-11 23:19:49 -0500

    feat: add database test

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import mysql from "mysql2";
 
-export default new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_DATABASE,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -12,3 +12,12 @@ export default new Sequelize(
         logging: false,
     }
 );
+
+try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+}
+
+export default sequelize;
