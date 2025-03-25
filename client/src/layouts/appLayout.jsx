b2aa92c4a42cd5da3d07f7007a31930e39446dbd
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-03-25 04:40:40 -0500

    feat: add accessibility widget

--- client/src/layouts/appLayout.jsx
+++ client/src/layouts/appLayout.jsx
@@ -5,10 +5,15 @@ import Footer from "./footer";
 // Toastify
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { AccessibilityWidget } from "sena-accessibility";
+import "sena-accessibility/dist/index.css";
 
 export default function AppLayout() {
     return (
         <>
+            <AccessibilityWidget
+                styles={{ "widget-primary": "#3b0764", "widget-secondary": "#7e22ce" }}
+            />
             <div className="flex flex-col min-h-screen">
                 <Header />
                 <main className="flex-1">
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
             "@pages": path.resolve(__dirname, "./src/pages"),
         },
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
 });
