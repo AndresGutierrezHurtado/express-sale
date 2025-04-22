54c27bd446a4455eefe73e3c902e69385094d53b
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-04-21 19:50:25 -0500

    style: improve layout styles

--- client/src/layouts/footer.jsx
+++ client/src/layouts/footer.jsx
@@ -4,12 +4,12 @@ export default function Footer() {
     return (
         <footer className="w-full px-3">
             <section className="w-full max-w-[1200px] mx-auto py-3">
-                <article className="w-full bg-gray-200/50 rounded-full px-5 py-2 font-bold text-gray-600/90 backdrop-blur">
-                    <div className="flex flex-col md:flex-row justify-between items-center">
+                <article className="w-full bg-base-200/50 border border-base-300/60 rounded-full px-5 py-2 font-bold backdrop-blur">
+                    <div className="flex flex-col md:flex-row justify-between items-center text-base-content/60">
                         <h3>&copy; Express Sale, 2024.</h3>
                         <Link
                             to="mailto:epxresssale.exsl@gmail.com"
-                            className="underline underline-offset-2"
+                            className="hover:underline underline-offset-2"
                         >
                             Contáctanos
                         </Link>
